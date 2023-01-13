5c65b6a3627221849044f5ac5e5449902b3e9a07
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-01-13 21:44:45 +0530

    update

--- Random Project/script.js
+++ Random Project/script.js
@@ -1,8 +1,13 @@
 let changeColor = document.querySelector('#change')
+let hide = document.querySelector("#hiddden");
 
 changeColor.onclick = () => {
     document.body.style.background = 'red';
 }
+
+ hide.onclick = () => {
+    console.log('hidden')
+ }
 
 // function color() {
 // changeColor('red');

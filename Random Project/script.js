39082c15b8f618ca4e52b52b6e84d843f1a6449b
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-01-24 21:31:48 +0530

    updated

--- Random Project/script.js
+++ Random Project/script.js
@@ -15,19 +15,19 @@ function hideBtn (){
     hide.style.display='none';
 }
 
-let obj = {1:'a',2:'b',3:'c'};
-const retrieveProperties = (obj = {}) => {
-    const res = [];
-    for(key in obj){
-       res.push( key );
-    };
-    return res;
- };
- console.log(retrieveProperties(obj));
+// let obj = {1:'a',2:'b',3:'c'};
+// const retrieveProperties = (obj = {}) => {
+//     const res = [];
+//     for(key in obj){
+//        res.push( key );
+//     };
+//     return res;
+//  };
+//  console.log(retrieveProperties(obj));
 
-// let obj = {id:"123",name:"candidate",age:"22",work:"sde"};
-//  let keys = Object.values(obj);
-//  console.log(keys);
+let obj = {id:"123",name:"candidate",age:"22",work:"sde"};
+ let keys = Object.values(obj);
+ console.log(keys);
  
  
 

f86f8c96e2a83668ef3bae0e7a48182cb1d4e815
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-02-03 18:46:00 +0530

    updated

--- Random Project/script.js
+++ Random Project/script.js
@@ -29,7 +29,20 @@ let obj = {id:"123",name:"candidate",age:"22",work:"sde"};
  let keys = Object.values(obj);
  console.log(keys);
  
- console.log(obj);
+//  console.log(obj);
 
 
+//  let arr=[1,2,3,4,5,6,7,8,9,10];
+//  let results=[];
+
+ function split(arr,N){
+    let results=[];
+    while(arr.length){
+        results.push(arr.splice(0,N));
+    }
+    return results;
+ }
+
+var ans = split([1,2,3,4,5,6,7,8,9,10],3);
+console.log(ans);
  

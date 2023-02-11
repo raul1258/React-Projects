7797193d6a251bb8ec4d6793aef8d7a86b3b4123
Rahul Jaiswal <rahul.jaiswal1997@gmail.com>
2023-02-11 21:36:47 +0530

    updated

--- Random Project/script.js
+++ Random Project/script.js
@@ -15,20 +15,19 @@ function hideBtn (){
     hide.style.display='none';
 }
 
-let obj = {1:'a',2:'b',3:'c'};
-const retrieveProperties = (obj = {}) => {
-    const res = [];
-    for(key in obj){
-       res.push( key );
-    };
-    return res;
- };
- console.log(retrieveProperties(obj));
+// let obj = {1:'a',2:'b',3:'c'};
+// const retrieveProperties = (obj = {}) => {
+//     const res = [];
+//     for(key in obj){
+//        res.push( key );
+//     };
+//     return res;
+//  };
+//  console.log(retrieveProperties(obj));
 
 let obj = {id:"123",name:"candidate",age:"22",work:"sde"};
  let keys = Object.values(obj);
  console.log(keys);
- 
  console.log(obj);
 
 
@@ -47,4 +46,3 @@ var ans = split([1,2,3,4,5,6,7,8,9,10],3);
 console.log(ans);
  
 
-// weather app
